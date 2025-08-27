be552ce8f64adf32d680f67985e1cf2bfaa1b8a8
JulieRaph <julie.tolve@gmail.com>
2025-08-27 16:39:20 +0200

    feat: Update PageHeader component with welcoming message

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,7 +4,8 @@
 </script>
 
 <template>
-    <h1 class="text-highlight-1">Julie TOLVE</h1>
+    <h1 class="text-highlight-1">Bienvenue sur mon site professionnel !<br>
+        Je suis ravie de votre visite et je vous souhaite une belle découverte.</h1>
 </template>
 
 <style scoped>
