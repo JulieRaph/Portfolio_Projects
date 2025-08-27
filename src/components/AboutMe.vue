f5a1680c2f1ed80b380b08d7a0cc1acc57ad9f76
JulieRaph <julie.tolve@gmail.com>
2025-08-27 12:50:20 +0200

    feat: Update AboutMe component with new experience details and technologies

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -7,10 +7,11 @@
         <h2 class="text-highlight-1 section-title">Qui suis-je ?</h2>
         <p>
             Je suis Julie Tolve, <span class="text-highlight-2">développeuse web</span> passionnée par la création d'applications web et mobiles. 
-            J'ai commencé mon parcours dans le développement en 2019 et depuis, j'ai acquis une solide expérience 
+            J'ai commencé mon parcours dans le développement en 2024 et depuis, je continue de me former et d'acquérir de nouvelles compétences dans l'objectif d'une solide expérience 
             dans la création de sites web dynamiques et interactifs.
-            J'ai travaillé sur divers projets, allant de la création de sites vitrines à des applications web complexes.
-            Mon expertise couvre une variété de technologies, notamment <span class="text-highlight-2">HTML, CSS, JavaScript, Vue.js et Node.js</span>.
+            J'ai travaillé sur divers projets, allant de la reproduction d'un site de location de logements tel que Airbnb, mais également dans 
+            la création de mon premier site communautaire ciblé que je déploie et maintient pour une interface utilisateur exceptionnelle et fluide.
+            Mon expertise couvre une variété de technologies, notamment <span class="text-highlight-2">Python, Flask, HTML, CSS, JavaScript, Vue.js et Node.js</span>.
             Je suis toujours à la recherche de nouveaux défis et d'opportunités pour apprendre et grandir en tant que développeuse.
         </p>
         <p><RouterLink to="/contact/1">Contactez moi</RouterLink></p>
