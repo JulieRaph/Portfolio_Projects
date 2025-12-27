5d15a03f41f8e6a685d47793fcd004fe830883f5
JulieRaph <julie.tolve@gmail.com>
2025-12-27 17:15:23 +0100

    feat: Update project descriptions and section title for clarity and consistency

--- src/components/MesProjets.vue
+++ src/components/MesProjets.vue
@@ -6,30 +6,30 @@
         {
             name: "WeRidUp",
             image: "project.png",
-            tags: "Vue.js;Flask;API RESTFul;Python;Typescript",
-            description: "Création d'un site communautaire pour les motards. Afin de rouler en toute liberté et en toute confiance avec d'autres passionnés de moto.",
-            tasks: "Création du backend en Flask API REST, sur Python via VSCode;Création de la partie Frontend en Vue.js;Création d'une messagerie en temps réel entre les utilisateurs."
+            tags: "Vue.js;Typescript;API RESTFul;Flask;MySql;Python",
+            description: "Création d'un site communautaire avec évènements pour les motards, incluant la conception d’une API REST et la gestion des données",
+            tasks: "Création du backend en Flask API REST, sur Python via VSCode;Création de la partie Frontend en Vue.js;Création d'une messagerie en temps réel entre les utilisateurs"
         },
          {
             name: "HBnb",
             image: "airbnb-logo.svg",
-            tags: "HTML;CSS;Javascript;Flask;API RESTFul;Python",
-            description: "Création d'un site personnalisé du célèbre site de location de logements Airbnb.",
-            tasks: "Création du backend en Flask API REST, sur Python via VSCode;Création de la partie Frontend en HTML, CSS et Javascript."
+            tags: "Javascript;HTML;CSS;API RESTFul;Flask;Python",
+            description: "Développement d'une application de location de logements, avec gestion des fonctionnalités côté serveur et interface utilisateur",
+            tasks: "Création du backend en Flask API REST, sur Python via VSCode;Création de la partie Frontend en HTML, CSS et Javascript"
         },
          {
             name: "Simple Shell",
             image: "shell.png",
             tags: "Langage C",
-            description: "Création d'un interpréteur de commandes en Langage C.",
-            tasks: "Création du nouvel interpréteur de commandes: En équipe de 2 personnes et en Méthode Agile."
+            description: "Réalisation d'un interpréteur de commandes en Langage C, avec gestion avancée des spécificateurs",
+            tasks: "Création du nouvel interpréteur de commandes: En équipe de 2 personnes et en Méthodologie Agile"
         },
     ]);
 </script>
 
 <template>
     <div id="projects">
-        <h2 class="text-highlight-1 section-title">Quelques projets</h2>
+        <h2 class="text-highlight-1 section-title">Projets & Solutions Développées</h2>
         <ProjectSingle v-for="(project, index) in projects" :key="project.name" :project="project" :reverse="index % 2 === 0 ? false : true"/>
     </div>
 </template>
