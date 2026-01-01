55e07d294fbb0580c351f9ae6baa3b2649e6d563
JulieRaph <julie.tolve@gmail.com>
2026-01-01 18:03:31 +0100

    feat: Update ContactPage layout with ContactHeader, ContactStatus, and ContactForm components

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,10 +1,17 @@
 <template>
+    <ContactHeader />
+    <ContactStatus />
+    <ContactForm />
     Page de contact {{ route.params.id }}
-    <RouterLink to="/">Retour à l'accueil</RouterLink>
+    <RouterLink to="/"><- Retour à l'accueil</RouterLink>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router';
+import { ref } from 'vue';
+import ContactHeader from '@/components/ContactHeader.vue';
+import ContactStatus from '@/components/ContactStatus.vue';
+import ContactForm from '@/components/ContactForm.vue';
 
 const route = useRoute()
 
