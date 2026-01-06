505b525f6705097691ecaa292a5f0fa8b3cef7b5
JulieRaph <julie.tolve@gmail.com>
2026-01-06 22:08:52 +0100

    feat: Add routes for CertificationsPage and CVPage

--- src/route.js
+++ src/route.js
@@ -1,8 +1,11 @@
+import CertificationsPage from "./pages/CertificationsPage.vue";
 import ContactPage from "./pages/ContactPage.vue";
+import CVPage from "./pages/CVPage.vue";
 import PortfolioMain from "./pages/PortfolioMain.vue";
 
 export const routes = [
     {path: "/", component: PortfolioMain},
     {path: "/contact", component: ContactPage},
-    {path: "/contact/:id", component: ContactPage},
+    {path: "/certifications", component: CertificationsPage},
+    {path: "/cv", component: CVPage},
 ]
