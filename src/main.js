87c319b98e67846ff48a2ac62a2c3cbb1d97e30e
JulieRaph <julie.tolve@gmail.com>
2026-01-06 20:32:59 +0100

    feat: Add scrollBehavior to router for consistent scroll position

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import { routes } from './route.js'
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        return { top: 0 }
+    }
 })
 
 const app = createApp(App)
